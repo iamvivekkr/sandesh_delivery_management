96003b90cb6d97f76b5af27928445c40310641ed
VIVEK KUMAR <91110678+iamvivekkr@users.noreply.github.com>
2025-07-19 01:21:45 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -50,7 +50,7 @@ const path = require("path");
 // Serve static frontend files
 app.use(express.static(path.join(__dirname, "../dist")));
 
-app.get("*", (req, res) => {
+app.get("/", (req, res) => {
   res.sendFile(path.resolve(__dirname, "../dist/index.html"));
 });
 
