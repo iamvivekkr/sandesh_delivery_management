89f7d85c5ef542cb6ae074dbda7cb39907c09cb5
VIVEK KUMAR <91110678+iamvivekkr@users.noreply.github.com>
2025-07-19 01:42:17 +0530

    Fix CORS and login response

--- server/server.js
+++ server/server.js
@@ -20,6 +20,8 @@ app.use(
         ? ["https://sandesh-delivery-management.onrender.com"]
         : ["http://localhost:5173", "http://localhost:3000"],
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
